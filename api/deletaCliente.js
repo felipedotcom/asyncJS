657920c4ab6ffba62725712c81fb570a02aebbf5
felipedotcom <felipe.rodolfo29@gmail.com>
2020-10-20 15:31:12 -0300

    m

--- api/deletaCliente.js
+++ api/deletaCliente.js
@@ -1,11 +1,3 @@
-/* export const deletaCliente = (id) => { 
-	  
-		.then(resposta => resposta.json())
-		.then(profile => profile)
-} */
-
-import { listaCliente } from "./listaCliente.js"
-
 export const deletaCliente = (id) => {
 	return fetch(`http://localhost:3000/profile/${id}`, {
 		method: "DELETE",
--- componentes/botao/deleta.js
+++
@@ -1,9 +0,0 @@
-export const botaoDeleta = () => {
-    const del = document.createElement('button')
-    
-    del.textContent = 'Deletar'
-    del.addEventListener('click', ()=> console.log('object'))
-    return del
-    
-}
-
